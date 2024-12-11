a18b7ede8fb0e121972ae55d7baf52d6165d1e14
FOODOHOLIC <adarshkohli183@gmail.com>
2024-12-11 21:38:34 +0530

    Resolved merge conflicts

--- MegaBlog/src/appwrite/configg.js
+++ MegaBlog/src/appwrite/configg.js
@@ -52,6 +52,82 @@ export class Service{
             throw error;
         }
     }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        }catch(error){
+            throw error;
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try{
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+                
+            )
+        }catch(error){
+            throw error;
+            return false;
+        }
+    }
+
+    //file upload services
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+
+            )
+        }catch(error){
+            throw error;
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try{
+            return await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId,
+
+            )
+            return true;
+        }catch(error){
+            throw error;
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service();
